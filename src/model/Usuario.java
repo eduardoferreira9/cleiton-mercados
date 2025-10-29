023fd76af0070d6b002fd6305d5d443740227b09
Eduardo <89710660+eduardoferreira9@users.noreply.github.com>
2025-10-29 07:29:45 -0300

    Create Usuario.java with initial comments
    Add initial comments for Usuario class structure.

---
+++ src/model/Usuario.java
@@ -0,0 +1,4 @@
+// Aqui podemos seguir com as classes abstratas
+// Onde teremos uma classe 'Usuário' com os atributos padrões
+// E depois podemos ter as classes 'Padrão' e 'Admnistrador', assim
+// Configurando as permissões de cada
