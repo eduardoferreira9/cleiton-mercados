ae91cfa7ac853565daaa9a0b5ef7efb874ea8994
ef02830@gmail.com <eduardoferreira9>
2025-10-29 07:57:13 -0300

    Create tabela usuários

---
+++ sql/database.sql
@@ -0,0 +1,7 @@
+-- Tabela de usuários do cleitin mercados
+CREATE TABLE usuarios (
+    id_usuarios SERIAL PRIMARY KEY,
+    usuario VARCHAR(50) NOT NULL UNIQUE,
+    senha VARCHAR (50) NOT NULL CHECK(length(senha) >= 8),
+    tipo_usuario INT CHECK(usuarios IN(1,2))
+);
